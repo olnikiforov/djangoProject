66ea759ea375846d19d0183c2caa67a4ec45baf0
olnikiforov <ikifircs@gmail.com>
2021-04-12 13:24:41 +0300

    hm8 done

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('djangoProject/', include('djangoProject.urls'))
 
 """
+import debug_toolbar
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('__debug__/', include(debug_toolbar.urls)),
 ]
